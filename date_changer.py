74ccfcc05ff1826e3b9794fe113d6b60e8999728
mikla2024 <amelkin.mihail@gmail.com>
2024-12-16 20:57:45 +0100

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,16 @@
+from datetime import date # import class 'date' from 'datetime' module
+
+username = 'mikla' # define username
+title = 'Note 1'	# def. name of note
+content = 'Conference call' # content of note
+status = 'open' # current status of the note
+created_date: date = date.today() # date of creation
+issued_date: date = date(2024,12,18) # deadline date
+
+print ('имя пользователя: ',username)
+print ('название: ',title)
+print ('содержание: ',content)
+print ('текущий статус: ',status)
+print('дата создания:',created_date.day) 				,'.',created_date.month)
+print ('дата истечения: ',issued_date.day,
+				'.',issued_date.month)
