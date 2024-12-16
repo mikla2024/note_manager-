c4e466dd3a84cd078f1e3087a2cb96b11585ef77
mikla2024 <amelkin.mihail@gmail.com>
2024-12-16 23:34:43 +0100

    Update date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -4,14 +4,13 @@ username = 'mikla' # define username
 title = 'Note 1'	# def. name of note
 content = 'Conference call' # content of note
 status = 'open' # current status of the note
-created_date: date = date.today() # date of creation, uses present date
+created_date: date = date.today() # date of creation
 issued_date: date = date(2024,12,18) # deadline date
 
 print ('имя пользователя: ',username)
 print ('название: ',title)
 print ('содержание: ',content)
-print ('текущий статус: ',status)
-print('дата создания:',created_date.day,
-	'.',created_date.month)	#show creat. date in 'dd.mm' form.
-print ('дата истечения: ',issued_date.day,
-	'.',issued_date.month)	#show issued date in 'dd.mm' form.
+print ('текущий статус: ',status)
+#returns string from date object, %-d - day, %b - month in short format 
+print ('дата создания:',created_date.strftime('%-d %b'))
+print ('дата истечения:',issued_date.strftime('%-d %b'))
