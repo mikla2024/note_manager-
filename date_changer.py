defc9576b700d5545f2989b270aa5962781310e3
mikla2024 <amelkin.mihail@gmail.com>
2024-12-17 00:09:09 +0100

    Update date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -1,11 +1,11 @@
-from datetime import date # import class 'date' from 'datetime' module
+from datetime import date,datetime # import classes 'date', 'datetime' from 'datetime' module
 
 username = 'mikla' # define username
 title = 'Note 1'	# def. name of note
 content = 'Conference call' # content of note
 status = 'open' # current status of the note
-created_date: date = date.today() # date of creation
-issued_date: date = date(2024,12,18) # deadline date
+created_date: date = date.today() # date of creation with time stamp
+issued_date: datetime = datetime(2024,12,18,15,45) # deadline date with time
 
 print ('имя пользователя: ',username)
 print ('название: ',title)
@@ -13,4 +13,4 @@ print ('содержание: ',content)
 print ('текущий статус: ',status)
 #returns string from date object, %-d - day, %b - month in short format 
 print ('дата создания:',created_date.strftime('%-d %b'))
-print ('дата истечения:',issued_date.strftime('%-d %b'))
+print ('дата истечения:',issued_date.strftime('%-d %b %H:%M'))
