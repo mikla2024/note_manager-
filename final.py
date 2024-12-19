cd026702d464d2c0b368d306145a8c9ba74fdeb9
mikla2024 <amelkin.mihail@gmail.com>
2024-12-20 00:11:37 +0100

    Add files via upload

--- final.py
+++ final.py
@@ -1,5 +1,16 @@
 # import classes 'date', 'datetime' from 'datetime' module
 from datetime import datetime
+
+
+# check unique title when add
+def f_uni(list_, title_):
+	if list_ is not None:
+		for title in list_:
+			if title_ == title:
+				return False
+	return True
+
+
 # dict. with notes
 notes = {}
 notes['username'] = input('Enter your name: ')
@@ -22,13 +33,16 @@ notes['issue_date'] = parsed_issue_date
 while True:
 	title_str = input('\nEnter title or leave this field empty.'
 																			'To continue press Enter: ')
-	if title_str != '': 
-		# if key doesnt exist it will be added as list
-		try:
-			notes['titles'].append(title_str)
-		except KeyError:
-			notes['titles'] = [title_str]
+	if title_str != '':
+		if f_uni(notes.get('titles'), title_str):
+			# if key doesnt exist in dict it will be added as list
+			notes.setdefault('titles', []).append(title_str)
+		else:
+			print('This title already exists')
 	else:
+		print('\nYour titles are:')
+		for title in notes.get('titles'):
+			print(title)
 		break
 
 # как вариант, можно сделать ввод заголовков
@@ -43,4 +57,4 @@ for key, value in notes.items():
 		print(f'{key}: {value:%-d %b}')
 		continue
 	print(f'{key}: {value}')
-
+	
